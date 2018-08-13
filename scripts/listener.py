f1248f8dfdaf93c34805b86ff9595a66dd430f74
w1211 <tom.wells96@gmail.com>
2018-08-13 15:48:12 +1000

    updates to beacon_finder

--- scripts/listener.py
+++ scripts/listener.py
@@ -17,24 +17,36 @@ from geometry_msgs.msg import Twist
 from cv_bridge import CvBridge, CvBridgeError
 
 
+class image_holder:
+    def __init__(self):
+        self.rgb_msg = None
+        self.depth_msg = None
 
-def callback(msg):
-    detect_beacon(bridge.imgmsg_to_cv2(msg, desired_encoding="passthrough"))
+        self.rgb_img = None
+        self.depth_img = None
 
-def callback2(msg):
-    depth_image = bridge.imgmsg_to_cv2(msg, desired_encoding="passthrough")
 
+def callback(rgb_msg):
+    print("got rgb,")
+    detect_beacon(bridge.imgmsg_to_cv2(rgb_msg, desired_encoding="passthrough"))
+
+def callback2(depth_msg):
+    print("got depth, height {}, encoding {}".format(depth_msg.height,depth_msg.encoding))
+    img_holder.depth_img = bridge.imgmsg_to_cv2(depth_msg, desired_encoding="8UC1")
+    #cv2.imshow("depth", depth_image)
+    img_holder.depth_msg = depth_msg
 
 
 def detect_beacon(img):
+
         #convert to HSV encoding - less affected by lighting
     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
     image = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
     # set up tools for detecting color
     kernel = np.ones((5,5),np.uint8)
-    upper_range = np.array([180,255,255])
-    lower_range = np.array([0,100,100])
+    upper_range = np.array([170,255,255])
+    lower_range = np.array([90,100,100])
 
     # apply blur, mask and erode /dilate to find all pink reigons
     mask = cv2.blur(hsv, (3,3))
@@ -52,49 +64,25 @@ def detect_beacon(img):
         x, y, w, h = cv2.boundingRect(c)
 
         # add all the large areas to results - filter out anu remaining noise
-        if w*h < 6000: #TODO change based on video quality
-            continue
+        #if w*h < 6000: #TODO change based on video quality
+
         centers.append((x+w/2,y+h/2))
 
 
         # for debug draw a green rectangle to visualize the bounding rect
         cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
-
+        depth = img_holder.depth_msg.data[(y+h/2)*img_holder.depth_msg.width + x+w/2]
+        print("DEpth is {}".format(depth))
 
     # only needed for debug
     cv2.imshow("RGB",image)
     cv2.imshow("Mask",mask)
     cv2.imshow("hsv",hsv)
+
+    cv2.imshow("depth", img_holder.depth_img)
     cv2.waitKey(2)
     return centers
 
-
-def stream_webcam():
-    camera_port = 0
-
-
-    #Number of frames to throw away while the camera adjusts to light levels
-    ramp_frames = 30
-
-    # Now we can initialize the camera capture object with the cv2.VideoCapture class.
-    # All it needs is the index to a camera port.
-    camera = cv2.VideoCapture(camera_port)
-
-    # Captures a single image from the camera and returns it in PIL format
-
-
-    print("Taking image...")
-    # Take the actual image we want to keep
-     # read is the   easiest way to get a full image out of a VideoCapture object.
-    retval, im = camera.read()
-    print("back")
-    # A nice feature of the imwrite method is that it will automatically choose the
-    # correct format based on the file extension you provide. Convenient!
-    cv2.imshow("tst", im)
-
-    # You'll want to release the camera, otherwise you won't be able to create a new
-    # capture object until your script exits
-    del(camera)
 
 
 def listener():
@@ -106,21 +94,25 @@ def listener():
     # run simultaneously.
     rospy.init_node('listener', anonymous=True)
 
-    #rospy.Subscriber('/camera/color/image_raw', Image, callback)
+    rospy.Subscriber('/camera/color/image_raw', Image, callback)
     rospy.Subscriber('/camera/depth/image_raw', Image, callback2)
     # spin() simply keeps python from exiting until this node is stopped
     print("waiting")
+
+
 if __name__ == '__main__':
     print ("here")
     if(len(sys.argv) > 1 and sys.argv[1] == "debug" ):
         stream_webcam()
 
     bridge = CvBridge()
+    img_holder = image_holder()
     vel_publisher = rospy.Publisher("/cmd_vel", Twist, queue_size=10)
     cmd_pub = rospy.Publisher("/cmd", String, queue_size=10)
     beacon_found = 0
-    depth_image = None
-    #imShowing = False
-    #win1 = cv2.imshow("win1",None)
+
+    # depth_image = None
+    # imShowing = False
+    # win1 = cv2.imshow("win1",None)
     listener()
     rospy.spin()
